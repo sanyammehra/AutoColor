53ad0a2a1baf0e0e66016da3d888b1bf782b716a
Kushaagra Goyal <kushaagragoyal@supersilicon.in>
2017-05-15 17:29:40 -0700

    Painting like results

--- src/tucha.py
+++ src/tucha.py
@@ -13,29 +13,83 @@ import tensorflow.contrib.slim as slim
 '''
 def baselinish(X,Y,is_training):
     
-    W_conv = tf.get_variable("Wconv",shape = [3,3,1,32])
+    W_conv = tf.get_variable("Wconv",shape = [7,7,1,32],initializer=tf.contrib.layers.xavier_initializer())
     b_conv = tf.get_variable("bconv",shape = [32])
     a1 = tf.nn.conv2d(X, W_conv, strides=[1,1,1,1], padding='SAME') + b_conv
     a1 = tf.nn.relu(a1)
-    #a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn1')
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn1')
     #a1 = tf.nn.max_pool(a1,ksize = [1,2,2,1],strides = [1,2,2,1],padding = 'VALID')
+
+    W_conv1 = tf.get_variable("Wconv1",shape = [3,3,32,32],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv1 = tf.get_variable("bconv1",shape = [32])
+    a1 = tf.nn.conv2d(a1, W_conv1, strides=[1,1,1,1], padding='SAME') + b_conv1
+    a1 = tf.nn.sigmoid(a1)
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn3')
     
-    W_conv2 = tf.get_variable("Wconv2",shape = [3,3,32,64])
+    W_conv2 = tf.get_variable("Wconv2",shape = [3,3,32,64],initializer=tf.contrib.layers.xavier_initializer())
     b_conv2 = tf.get_variable("bconv2",shape = [64])
     a1 = tf.nn.conv2d(a1, W_conv2, strides=[1,1,1,1], padding='SAME') + b_conv2
     a1 = tf.nn.relu(a1)
-    #a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn2')
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn2')
     
-    W_conv3 = tf.get_variable("Wconv3",shape = [3,3,64,3])
-    b_conv3 = tf.get_variable("bconv3",shape = [3])
+    W_conv3 = tf.get_variable("Wconv3",shape = [1,1,64,32],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv3 = tf.get_variable("bconv3",shape = [32])
     a1 = tf.nn.conv2d(a1, W_conv3, strides=[1,1,1,1], padding='SAME') + b_conv3
+    a1 = tf.nn.softmax(a1,dim = 3)
+    W_conv4 = tf.get_variable("Wconv4",shape = [1,1,32,3],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv4 = tf.get_variable("bconv4",shape = [3])
+    a1 = tf.nn.conv2d(a1, W_conv4, strides=[1,1,1,1], padding='SAME') + b_conv4
     
+
+
     return a1
     
     #a1 = tf.nn.relu(a1)
     #a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn3')
 
-def lrelu(x, leak=0, name='lrelu'):
+
+
+def baselinish2(X,is_training):
+
+    Y = tf.image.convert_image_dtype(X,tf.float32)
+    inputt = tf.image.rgb_to_hsv(Y)
+    inpp = inputt[:,:,:,1:3]
+    inp = inputt[:,:,:,0:1]
+    
+    W_conv = tf.get_variable("Wconv",shape = [7,7,1,32],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv = tf.get_variable("bconv",shape = [32])
+    a1 = tf.nn.conv2d(inp, W_conv, strides=[1,1,1,1], padding='SAME') + b_conv
+    a1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn1')
+    #a1 = tf.nn.max_pool(a1,ksize = [1,2,2,1],strides = [1,2,2,1],padding = 'VALID')
+
+    W_conv1 = tf.get_variable("Wconv1",shape = [3,3,32,32],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv1 = tf.get_variable("bconv1",shape = [32])
+    a1 = tf.nn.conv2d(a1, W_conv1, strides=[1,1,1,1], padding='SAME') + b_conv1
+    a1 = tf.nn.sigmoid(a1)
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn3')
+    
+    W_conv2 = tf.get_variable("Wconv2",shape = [3,3,32,64],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv2 = tf.get_variable("bconv2",shape = [64])
+    a1 = tf.nn.conv2d(a1, W_conv2, strides=[1,1,1,1], padding='SAME') + b_conv2
+    a1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.batch_norm(a1,center = True, scale = True, is_training = is_training,scope = 'bn2')
+    
+    W_conv3 = tf.get_variable("Wconv3",shape = [1,1,64,32],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv3 = tf.get_variable("bconv3",shape = [32])
+    a1 = tf.nn.conv2d(a1, W_conv3, strides=[1,1,1,1], padding='SAME') + b_conv3
+    a1 = tf.nn.relu(a1)
+    W_conv4 = tf.get_variable("Wconv4",shape = [1,1,32,2],initializer=tf.contrib.layers.xavier_initializer())
+    b_conv4 = tf.get_variable("bconv4",shape = [2])
+    a1 = tf.nn.conv2d(a1, W_conv4, strides=[1,1,1,1], padding='SAME') + b_conv4
+
+    a3 = tf.concat((inp,a1),axis = 3)
+    a2 = tf.image.hsv_to_rgb(a3)
+
+    
+    return a1,a2,inpp,Y
+
+def lrelu(x, leak=0.2, name='lrelu'):
     return tf.maximum(leak*x, x)
 
 
@@ -83,20 +137,58 @@ def complex_pokemon_model(gray_image,train=True):
    return conv18
 
 
-def simpler_model(gray_image,train = False):
-  conv1 = lrelu(slim.convolution(gray_image, 32, 3, stride=1, scope='conv1', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+def complex_pokemon_model2(X,train=True):
+
+  Y = tf.image.convert_image_dtype(X,tf.float32)
+  inputt = tf.image.rgb_to_hsv(Y)
+  inpp = inputt[:,:,:,1:3]
+  inp = inputt[:,:,:,0:1]
+
+  conv1 = lrelu(slim.convolution(inp, 32, 3, stride=1, scope='conv1', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv2 = lrelu(slim.convolution(conv1, 32, 3, stride=1, scope='conv2', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv3 = lrelu(slim.convolution(conv2, 64, 3, stride=1, scope='conv3', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
-  conv5 = lrelu(slim.convolution(conv3, 128, 3, stride=1, scope='conv5', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
-  conv12 = lrelu(slim.convolution(conv5, 64, 1, stride=1, scope='conv12', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv4 = lrelu(slim.convolution(conv3, 64, 3, stride=1, scope='conv4', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv5 = lrelu(slim.convolution(conv4, 128, 3, stride=1, scope='conv5', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv6 = lrelu(slim.convolution(conv5, 128, 3, stride=1, scope='conv6', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv7 = lrelu(slim.convolution(conv6, 256, 3, stride=1, scope='conv7', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv8 = lrelu(slim.convolution(conv7, 256, 3, stride=1, scope='conv8', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv9 = lrelu(slim.convolution(conv8, 128, 3, stride=1, scope='conv9', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv10 = lrelu(slim.convolution(conv9, 128, 3, stride=1, scope='conv10', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv11 = lrelu(slim.convolution(conv10, 64, 1, stride=1, scope='conv11', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  conv12 = lrelu(slim.convolution(conv11, 64, 1, stride=1, scope='conv12', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv13 = lrelu(slim.convolution(conv12, 32, 1, stride=1, scope='conv13', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv14 = lrelu(slim.convolution(conv13, 32, 1, stride=1, scope='conv14', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv15 = lrelu(slim.convolution(conv14, 16, 1, stride=1, scope='conv15', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv16 = lrelu(slim.convolution(conv15, 16, 1, stride=1, scope='conv16', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
   conv17 = lrelu(slim.convolution(conv16, 8, 1, stride=1, scope='conv17', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
-  if train: conv17 = tf.nn.dropout(conv17, 0.8)
+  #if train: conv17 = tf.nn.dropout(conv17, 0.8)
+  conv18 = lrelu(slim.convolution(conv17, 2, 1, stride=1, scope='conv18', normalizer_fn=slim.batch_norm, activation_fn=tf.identity))
+  #if train: conv18 = tf.nn.dropout(conv18, 0.8)
+
+  a3 = tf.concat((inp,conv18),axis = 3)
+  a2 = tf.image.hsv_to_rgb(a3)
+
+    
+  return conv18,a2,inpp,Y
+   
+  
+
+
+def simpler_model(gray_image,train = True):
+  #bn = lambda x: slim.batch_norm(x, 'is_training' =train)
+  conv1 = lrelu(slim.convolution(gray_image, 32, 3, stride=1, scope='conv1', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train}, activation_fn=tf.identity))
+  conv2 = lrelu(slim.convolution(conv1, 32, 3, stride=1, scope='conv2', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  conv3 = lrelu(slim.convolution(conv2, 64, 3, stride=1, scope='conv3', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  conv5 = lrelu(slim.convolution(conv3, 128, 3, stride=1, scope='conv5', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  conv12 = lrelu(slim.convolution(conv5, 64, 1, stride=1, scope='conv12', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  conv13 = lrelu(slim.convolution(conv12, 32, 1, stride=1, scope='conv13', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  conv14 = lrelu(slim.convolution(conv13, 32, 1, stride=1, scope='conv14', normalizer_fn=slim.batch_norm,normalizer_params = {'is_training':train}, activation_fn=tf.identity))
+  conv15 = lrelu(slim.convolution(conv14, 16, 1, stride=1, scope='conv15', normalizer_fn=slim.batch_norm,normalizer_params = {'is_training':train}, activation_fn=tf.identity))
+  conv16 = lrelu(slim.convolution(conv15, 16, 1, stride=1, scope='conv16', normalizer_fn=slim.batch_norm,normalizer_params = {'is_training':train}, activation_fn=tf.identity))
+  conv17 = lrelu(slim.convolution(conv16, 8, 1, stride=1, scope='conv17', normalizer_fn=slim.batch_norm, normalizer_params = {'is_training':train},activation_fn=tf.identity))
+  #if train: conv17 = tf.nn.dropout(conv17, 0.8)
   conv18 = (slim.convolution(conv17, 2, 1, stride=1, scope='conv18', activation_fn=tf.identity))
-  if train: conv18 = tf.nn.dropout(conv18, 0.8)
+  #if train: conv18 = tf.nn.dropout(conv18, 0.8)
   
   return conv18
 
