dfcd57676d554958722277d4656739fb5b7446c2
Yash Malviya <ymalviya@stanford.edu>
2017-06-11 23:52:34 +0000

    trained classification model

--- src/Loss_metrics.py
+++ src/Loss_metrics.py
@@ -44,17 +44,17 @@ def assign_prob(NN, y):
     # y is [N, H, W, 3]
     prob_dist = np.zeros((y.shape[0]*y.shape[1]*y.shape[2], 400))
     NN = np.reshape(NN, [NN.size,])
-    prob_dist[range(y.shape[0]*y.shape[1]*y.shape[2]),NN] = 1
+    #prob_dist[range(y.shape[0]*y.shape[1]*y.shape[2]),NN] = 1
     
-    #prob_dist[range(y.shape[0]*y.shape[1]*y.shape[2]),NN] = 0.12
-    #prob_dist[range(NN[NN<399].size),NN[NN<399]+1] = 0.11
-    #prob_dist[range(NN[NN>0].size),NN[NN>0]+1] = 0.11
-    #prob_dist[range(NN[NN<380].size),NN[NN<380]+1] = 0.11
-    #prob_dist[range(NN[NN>20].size),NN[NN>20]+1] = 0.11
-    #prob_dist[range(NN[NN<379].size),NN[NN<379]+1] = 0.11
-    #prob_dist[range(NN[NN>21].size),NN[NN>21]+1] = 0.11
-    #prob_dist[range(NN[NN<381].size),NN[NN<381]+1] = 0.11
-    #prob_dist[range(NN[NN>19].size),NN[NN>19]+1] = 0.11    
+    prob_dist[range(y.shape[0]*y.shape[1]*y.shape[2]),NN] = 0.12
+    prob_dist[range(NN[NN<399].size),NN[NN<399]+1] = 0.11
+    prob_dist[range(NN[NN>0].size),NN[NN>0]+1] = 0.11
+    prob_dist[range(NN[NN<380].size),NN[NN<380]+1] = 0.11
+    prob_dist[range(NN[NN>20].size),NN[NN>20]+1] = 0.11
+    prob_dist[range(NN[NN<379].size),NN[NN<379]+1] = 0.11
+    prob_dist[range(NN[NN>21].size),NN[NN>21]+1] = 0.11
+    prob_dist[range(NN[NN<381].size),NN[NN<381]+1] = 0.11
+    prob_dist[range(NN[NN>19].size),NN[NN>19]+1] = 0.11    
     return np.reshape(prob_dist,[y.shape[0], y.shape[1], y.shape[2], 400])
 
 def Prob_dist1(y):
@@ -83,12 +83,33 @@ def YUV2rgb(UV, y):
     # UV is (N, H, W, 400)
     # returns RGB values corresponding to this YUV input
     inv_mat  = np.array([[1,0,1.13983],[1,-0.39465,-0.58060],[1,2.03211,0]])
-    UV_arg_max = np.argmax(UV,axis = 3)
-    UV_arg_row_number = UV_arg_max//20
-    UV_arg_column_number = UV_arg_max%20
-    UV_val_a = -0.6 + UV_arg_row_number*1.2/19
-    UV_val_b = -0.6 + UV_arg_column_number*1.2/19
-    YUV_output = np.concatenate((y[...],UV_val_a[...,np.newaxis],UV_val_b[...,np.newaxis]),axis = 3)
+    UV_arg_max = np.argmax(UV, axis=3)
+    size_list = UV_arg_max.shape
+    UV_t = np.exp(UV)/np.sum(np.exp(UV),axis=3)[...,np.newaxis]
+    #UV_t = UV + 1e-6
+    UV_a = np.zeros_like(UV_arg_max, dtype=np.float64)
+    UV_b = np.zeros_like(UV_arg_max, dtype=np.float64)
+    norm_a = np.zeros_like(UV_arg_max, dtype=np.float64)
+    norm_b = np.zeros_like(UV_arg_max, dtype=np.float64)
+    # code for taking mean
+    #for i in range(20):
+    #    for j in range(20):
+    #        UV_a += (-0.6+i*1.2/19+1.2/40)*UV_t[:,:,:,i*20+j]
+    #        UV_b += (-0.6+j*1.2/19+1.2/40)*UV_t[:,:,:,i*20+j]
+    # Code for taking mode
+    for i in range(20):
+        for j in range(20):
+              UV_a += (-0.6+i*1.2/19)*np.exp(np.log(UV_t[:,:,:,i*20+j])/0.38)
+              UV_b += (-0.6+j*1.2/19)*np.exp(np.log(UV_t[:,:,:,i*20+j])/0.38)
+              norm_a += np.exp(np.log(UV_t[:,:,:,i*20+j])/0.38)
+    UV_a = UV_a/norm_a
+    UV_b = UV_b/norm_a
+    #UV_arg_row_number = UV_arg_max//20
+    #UV_arg_column_number = UV_arg_max%20
+    #UV_a = -0.6 + UV_arg_row_number*1.2/19 + 1.2/40
+    #UV_b = -0.6 + UV_arg_column_number*1.2/19 + 1.2/40
+    # Code for soft annealing
+    YUV_output = np.concatenate((y[...],UV_a[..., np.newaxis],UV_b[..., np.newaxis]),axis = 3)
     RGB_output = YUV_output.dot(inv_mat)
     RGB_output[RGB_output>1] = 1
     RGB_output[RGB_output<0] = 0
